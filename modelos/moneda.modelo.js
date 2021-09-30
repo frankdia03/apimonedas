95995c239fd75a196756dc816d053d95ca4c19d2
frankdia03 <frankdia03@gmail.com>
2021-09-29 18:34:55 -0700

    update paises controaldor

--- modelos/moneda.modelo.js
+++ modelos/moneda.modelo.js
@@ -23,21 +23,21 @@ Moneda.obtener = (idMoneda, resultado) => {
             resultado(err, null);
             return;
         }
-            //la consulta devuelve resultado
+        //la consulta devuelve resultado
 
-            if(res.length){
-                console.log("Moneda encontrada:", res[0]);
-                resultado(null, res[0]);
-                return;
+        if (res.length) {
+            console.log("Moneda encontrada:", res[0]);
+            resultado(null, res[0]);
+            return;
         }
         // no se en encutraron registros
-        resultado({tipo: "No encontrado"}, null);
+        resultado({ tipo: "No encontrado" }, null);
 
     });
 }
 
 //metodo que obtiene la lista de monedas
-Moneda.listar=(resultado)=>{
+Moneda.listar = (resultado) => {
     sql.query('CALL spListarMonedas', (err, res) => {
         // las tildes al revez se usan para cadenas de texto con parametros. se 
         //mezclan partes fijas y partes variables  
@@ -48,58 +48,58 @@ Moneda.listar=(resultado)=>{
             resultado(err, null);
             return;
         }
-            //la consulta devuelve resultado
+        //la consulta devuelve resultado
 
-                console.log("Lista de Monedas encontrada:", res[0]);
-                resultado(null, res[0]);
-        });
+        console.log("Lista de Monedas encontrada:", res[0]);
+        resultado(null, res[0]);
+    });
 }
 
 //metodo que obtiene un registro basado en la clave primaria
 Moneda.actualizar = (moneda, resultado) => {
     sql.query('CALL spActualizarMoneda(?????)', //consulta sql (err, res) => {
-    [moneda.id, moneda.moneda, moneda.sigla, moneda.simbolo, moneda.emisor], //parametros 
-    (err, res) =>{
+        [moneda.id, moneda.moneda, moneda.sigla, moneda.simbolo, moneda.emisor], //parametros 
+        (err, res) => {
 
-        //verificar si hubo error ejecutando la consulta
-        if (err) {
-            console.log("Error actualizando moneda:", err);
-            resultado(err, null);
-            return;
-        }
+            //verificar si hubo error ejecutando la consulta
+            if (err) {
+                console.log("Error actualizando moneda:", err);
+                resultado(err, null);
+                return;
+            }
 
             //la consulta no affecto filas
-            if(res.affectedRows==0){
+            if (res.affectedRows == 0) {
                 //no se encontrarobn registros
-                resultado({tipo: "No encontrado"},null);
+                resultado({ tipo: "No encontrado" }, null);
                 return;
-        }
+            }
 
-        console.log("Moneda actualizada:", moneda);
-        resultado(null, {moneda});
+            console.log("Moneda actualizada:", moneda);
+            resultado(null, { moneda });
 
-    });
+        });
 }
 
 //metodo que elimina un registro
 Moneda.eliminar = (idMoneda, resultado) => {
-    sql.query('DELETE * FROM Moneda WHERE Id=?', idMoneda (err, res) => {
+    sql.query('DELETE * FROM Moneda WHERE Id=?', idMoneda(err, res)=> {
 
         //verificar si hubo error ejecutando la consulta
-        if (err) {
+        if(err) {
             console.log("Error eliminando la moneda:", err);
             resultado(err, null);
             return;
-        
+        }
             //la consulta no afecto registyro
 
-            if(res.affectedRows==0){
-                resultado({tipo: "No encontrado"}, null);
-                return;
-        }
-        // no se en encutraron registros
-        console.log("Error eliminando la moneda:", err);
-        resultado({tipo: "No encontrado"}, null);
+        if (res.affectedRows == 0){
+        resultado({ tipo: "No encontrado" }, null);
+        return;
+    }
+    // no se en encutraron registros
+    console.log("Error eliminando la moneda:", idMoneda);
+    resultado(null, res);
 
     });
 }
